68947dc5810043688ea67e875070666a7c55537d
Tomás Pollak <tomaspollak@gmail.com>
2012-12-24 16:33:01 -0300

    Converted tabs to spaces. Use array for command arguments.

--- index.js
+++ index.js
@@ -3,48 +3,59 @@ var join = require('path').join,
 
 var Dialog = module.exports = {
 
-	info: function(str, title, callback){
-		this.show('info', str, title, callback);
-	},
+  info: function(str, title, callback){
+    this.show('info', str, title, callback);
+  },
 
-	warn: function(str, title, callback){
-		this.show('warning', str, title, callback);
-	},
+  warn: function(str, title, callback){
+    this.show('warning', str, title, callback);
+  },
 
-	show: function(type, str, title, callback){
+  show: function(type, str, title, callback){
 
-		if (typeof title == 'function') {
-			callback = title;
-			title = null;
-		}
+    if (typeof title == 'function') {
+      callback = title;
+      title = null;
+    }
 
-		var cmd, os_name = process.platform, title = title ? title : 'Important';
+    var cmd = [],
+        os_name = process.platform,
+        title = title ? title : 'Important';
 
-		// remove or replace unsupported chars.
-		var str = str.replace(/(`|<|>|\|)/g, '').replace(/"/g, '\\"');
+    // remove or replace unsupported chars.
+    var str = str.replace(/(`|<|>|\|)/g, '').replace(/"/g, '\\"');
 
-		if (os_name == 'linux'){
+    if (os_name == 'linux'){
 
-			var width = str.length > 30 ? "--width 300" : '';
-			cmd = 'zenity ' + width  + ' --' + type + ' --text "' + str + '"';
-			cmd += ' --title "' + title + '"';
+      cmd.push('zenity');
+      cmd.push('--' + type);
+      cmd.push('--text') && cmd.push('\"' + str + '\"');
+      cmd.push('--title') && cmd.push('\"' + title + '\"');
+      if (str.length > 30) cmd.push('--width') && cmd.push('300');
 
-		} else if (os_name == 'darwin'){
+    } else if (os_name == 'darwin'){
 
-			cmd = "osascript -e 'tell app \"System Events\" to display dialog ";
-			cmd += '"' + str + '" with title "' + title + '" buttons "OK"';
-			cmd += (type == 'warning') ? " with icon 0'" : "'";
+      cmd.push('osascript') && cmd.push('-e');
+      var str = 'tell app \"System Events\" to display dialog';
+      str += '\"' + str + '\" with title \"' + title + '\" buttons "OK"';
+      str += (type == 'warning') ? " with icon 0'" : "'";
+      cmd.push(str);
 
-		} else {
+    } else {
 
-			// msgbox.vbs script from http://stackoverflow.com/questions/774175
-			cmd = 'cscript ' + join(__dirname, 'msgbox.vbs');
-			cmd += ' "' + [title, str].join('" "') + '"';
+      // msgbox.vbs script from http://stackoverflow.com/questions/774175
+      cmd.push('cscript');
+      cmd.push(join(__dirname, 'msgbox.vbs'));
+      cmd.push('"' + [title, str].join('" "') + '"');
 
-		}
+    }
 
-		exec(cmd, callback);
+    this.run(cmd, callback);
 
-	}
+  },
+
+  run: function(cmd, cb){
+    exec(cmd.join(' '), cb);
+  }
 
 }
