84b13020975746786137457c4399eed855322076
Tomás Pollak <tomaspollak@gmail.com>
2014-03-31 20:15:50 -0700

    Syntax in bin/dialog.

--- bin/dialog.js
+++ bin/dialog.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-function abort(msg){
-	console.log(msg) || process.exit(1);
+function abort(msg) {
+  console.log(msg) || process.exit(1);
 }
 
-if(!process.argv[3])
-	abort("Usage: show_dialog [info|warn] [message]")
+if (!process.argv[3])
+  abort("Usage: show_dialog [info|warn] [message]")
 
-var dialog = require('./..');
-var type = process.argv[2];
-var message = process.argv.slice(3).join(' ');
+var dialog  = require('./..'),
+    type    = process.argv[2],
+    message = process.argv.slice(3).join(' ');
 
 if (!dialog[type])
-	abort("Unknown message type: " + type);
-else
-	dialog[type](message, function(err){
-		if (err) console.log(err.message.trim());
-	});
+  return abort("Unknown message type: " + type);
+
+dialog[type](message, function(err) {
+  if (err) console.log(err.message.trim());
+});
